172856a719c0c742e0a8a3f7f2f2b7042b332133
ABHISHEK GARG <abgarg@adobe.com>
2020-04-01 09:33:03 +0530

    Run Linting

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -24,7 +24,8 @@ CORS(app)
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks')
@@ -46,7 +47,8 @@ def fetch_drinks():
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks-detail')
@@ -68,7 +70,8 @@ def fetch_drinks_detail(payload):
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+        returns status code 200 and json {"success": True, "drinks": drink}
+        where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks', methods=['POST'])
@@ -92,7 +95,7 @@ def create_drink(payload):
             'drinks': drink
         })
 
-    except:
+    except Exception:
         abort(404)
 
 
@@ -104,7 +107,8 @@ def create_drink(payload):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        returns status code 200 and json {"success": True, "drinks": drink}
+        where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<id>', methods=['PATCH'])
@@ -128,7 +132,7 @@ def update_drink_by_id(payload, id):
             'drinks': [drink.long()]
         })
 
-    except:
+    except Exception:
         abort(404)
 
     '''
@@ -138,7 +142,8 @@ def update_drink_by_id(payload, id):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        returns status code 200 and json {"success": True, "delete": id}
+        where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<id>', methods=['DELETE'])
@@ -156,7 +161,7 @@ def delete_drink_by_id(payload, id):
             'delete': drink.id
         })
 
-    except:
+    except Exception:
         abort(404)
 
 
@@ -177,7 +182,7 @@ def unprocessable(error):
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 404,
                     "message": "resource not found"
                     }), 404
@@ -186,7 +191,7 @@ def unprocessable(error):
 
 '''
 @TODO implement error handler for 404
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 
 
@@ -201,7 +206,7 @@ def not_found(error):
 
 '''
 @TODO implement error handler for AuthError
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 @app.errorhandler(AuthError)
 def auth_error(error):
